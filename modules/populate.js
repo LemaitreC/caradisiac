33a371e04d8771bdc247c1c1dd899de7189861c1
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2018-03-23 10:30:11 +0100

    feat(populate) : added populate module

---
+++ modules/populate.js
@@ -0,0 +1,73 @@
+
+const elasticsearch = require('elasticsearch');
+const pReflect = require('p-reflect');
+const pMap = require('p-map');
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+
+exports.addCars = callback =>{
+  getBrands().then(res=>{
+   let promises=[]
+
+   for(var i=0;i<5;i++){
+     promises.push(getModels(res[i]))
+   }
+  return pMap(promises, pReflect,{concurrency:2})
+  }).then(res=>{
+    const results = res.filter(x => x.isFulfilled).map(x => x.value);
+    const r = res.filter(x => x.isRejected).map(x => x.value);
+    let models=[]
+
+    for(var i=0 ; i<results.length;i++){
+      for(var j = 0 ; j<results[i].length; j++){
+        models.push(results[i][j])
+      }
+    }
+    return elasticsearchBulk(models)
+  }).then(()=>{
+     callback(null,"The models were inserted properly")
+  }).catch(err=>{
+    callback(err)
+  })
+}
+
+function elasticsearchBulk(models){
+  let bulkBody = [];
+  models.forEach(item => {
+    bulkBody.push({
+      index: {
+        _index: "cars",
+        _type: "car",
+        _id: item.uuid
+      }
+    });
+
+    bulkBody.push(item);
+  });
+
+  var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+  });
+
+  client.ping({
+    // ping usually has a 3000ms timeout
+    requestTimeout: 1000
+  }, function (error) {
+    if (error) {
+      console.trace('elasticsearch cluster is down!');
+    } else {
+      console.log('All is well');
+    }
+  });
+
+  client.bulk({
+      body: bulkBody
+  }, function (error, response) {
+      if (error) {
+          console.error(error);
+          return;
+      }
+  });
+
+}
