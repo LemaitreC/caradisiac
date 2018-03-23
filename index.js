760122a6c12127f0fa02aeb0d9e70d997c2f13c9
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2018-03-23 10:29:37 +0100

    feat(routes): add populate route

--- index.js
+++ index.js
@@ -1,94 +1,42 @@
+const populate = require('./modules/populate')
+const restify = require('restify')
+const server = restify.createServer()
 
-const elasticsearch = require('elasticsearch');
-const pReflect = require('p-reflect');
-const pMap = require('p-map');
-const {getBrands} = require('node-car-api');
-const {getModels} = require('node-car-api');
-
-function index(){
-  getBrands().then(res=>{
-   let promises=[]
-
-   for(var i=0;i<2;i++){
-     console.log(res[i])
-     promises.push(getModels(res[i]))
-   }
-  //  console.log("Test")
-  return pMap(promises, pReflect,{concurrency:2})
-  }).then(res=>{
-    const results = res.filter(x => x.isFulfilled).map(x => x.value);
-    const r = res.filter(x => x.isRejected).map(x => x.value);
-    let brand=[]
-
-    for(var i=0 ; i<results.length;i++){
-      for(var j = 0 ; j<results[i].length; j++){
-        brand.push(results[i][j])
-      }
-    }
-
-    console.log(brand)
-    console.log("rejected :" +r.length)
-  }).catch(err=>{
-    console.log(err)
-  })
+const status = {
+	'ok': 200,
+	'created': 201,
+	'noContent': 204,
+	'notModified': 304,
+	'badRequest': 400,
+	'unauthorised': 401,
+	'notFound': 404
 }
 
-index()
+const defaultPort = 8081
+
+server.get('/populate', function(req, res) {
+	populate.addCars( (err, response) => {
+		res.setHeader('content-type', 'application/json')
+		res.setHeader('accepts', 'GET')
+		if (err) {
+			res.send(status.badRequest, {
+				error: err.message
+			})
+		} else {
+			res.send(status.ok, response)
+		}
+		res.end()
+	})
+})
 
 
-function fetchModels(brand){
-  return new Promise(function(resolve,reject){
-    getModels(brand).then(res=>{
-      // let newRes = []
-      // for(var i=0; i<res.length;i++){
-      //   if(res.length != 0) newRes.push(res[i])
-      // }
 
-      resolve(res)
-    }).catch(err=>{
-      reject(err)
-    })
-  })
-}
+const port = process.env.PORT || defaultPort
 
-// getModels('PEUGEOT').then(res=>{
-//   console.log(res)
-// }).catch(err=>{
-//   console.log(err)
-// })
-
-// var client = new elasticsearch.Client({
-//   host: 'localhost:9200',
-//   log: 'trace'
-// });
-//
-// client.ping({
-//   // ping usually has a 3000ms timeout
-//   requestTimeout: 1000
-// }, function (error) {
-//   if (error) {
-//     console.trace('elasticsearch cluster is down!');
-//   } else {
-//     console.log('All is well');
-//   }
-// });
-//
-// var body = [];
-// for (var i = 0; i < stocks.length; i++ ) {
-//     delete stocks[i]._id;
-//     var config = { index:  { _index: 'stocks', _type: 'stock', _id: i } };
-//     body.push(config);
-//     body.push(stocks[i]);
-// }
-//
-// client.bulk({
-//     body: body
-// }, function (error, response) {
-//     if (error) {
-//         console.error(error);
-//         return;
-//     }
-//     else {
-//         console.log(response);  //  I don't recommend this but I like having my console flooded with stuff.  It looks cool.  Like I'm compiling a kernel really fast.
-//     }
-// });
+server.listen(port, err => {
+	if (err) {
+		console.error(err)
+	} else {
+		console.log('App is ready at : ' + port)
+	}
+})
